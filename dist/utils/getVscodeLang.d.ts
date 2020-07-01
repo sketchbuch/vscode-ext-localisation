89e568fd04872f952f36a0830e5b0033799c162d
SketchBuch <sb.design.de@gmail.com>
2020-07-01 17:30:59 +0200

    chore: remove dist

--- dist/utils/getVscodeLang.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const getVscodeLang: (vscodeLangConfig: string | undefined) => string;
--- dist/utils/loadTranslations.d.ts
+++
@@ -1,3 +0,0 @@
-import { Translations } from '../localisation.interface';
-export declare let translations: Translations;
-export declare const loadTranslations: (lang: string, extensionPath: string) => void;
